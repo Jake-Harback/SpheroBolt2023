4666e2ba0b0071eb0d4506fa2cf11d4b9169a3cb
Jake Harback <01jharback@gmail.com>
2023-02-07 08:37:33 -0500

    Hello World

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,3 +1,13 @@
 //Programmer: Jake Harback 
 //Date: 2.6.2023
 //Project: Sphero Bolt
+
+
+//hello world practice program
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+}
