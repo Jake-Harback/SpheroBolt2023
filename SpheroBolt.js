aa6135af3c85371f7012aca2a9aa816f493462ba
Jake Harback <01jharback@gmail.com>
2023-02-13 08:37:53 -0500

    2.13.23

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,3 +1,15 @@
 //Team: Jake Harback, Andrew Krusniak 
 //Date: 2.13.2023
-//Project: Sphero Bolt Maze
+//Project: Sphero Bolt Maze 
+
+async function startProgram() { 
+	await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true)
+	setSpeed(80); 
+	await delay (2); 
+	setSpeed(0); 
+	await roll((getHeading() + 90), 60, 0); 
+	setMainLed({r: 0, g: 0, b: 255 }); 
+
+
+
+
