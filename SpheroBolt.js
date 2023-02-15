823dd9ebfe3abcce9105386e90fe7cc2edc60736
Jake Harback <01jharback@gmail.com>
2023-02-15 08:29:23 -0500

    2.15.23

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -5,14 +5,31 @@
 
 //blue 
 
-async function startProgram() { 
+async function blue() {
     await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true)
-    setSpeed(70); 
-    await delay (2); 
-    setSpeed(0); 
+    await roll(0, 70, 2);
     setHeading(90);
     setMainLed({r: 0, g: 0, b: 255 });  
     
 } 
 
-//sound 1
+//sound 1 
+
+async function firstSound() {
+	await roll(0, 45, 2); 
+	setHeading(90); 
+	await roll(0, 25, 2); 
+	await Sound.Mechanical.TruckHorn.play(); 
+	
+}
+
+
+
+
+
+
+async function startProgram() { 
+	await blue();
+	await firstSound(); 
+	
+}  
