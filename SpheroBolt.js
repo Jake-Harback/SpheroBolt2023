7db8d657f089f74229b1e93f210038f2fac6c971
Jake Harback <01jharback@gmail.com>
2023-02-15 07:36:22 -0500

    Blue

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,15 +1,18 @@
-//Team: Jake Harback, Andrew Krusniak 
-//Date: 2.13.2023
-//Project: Sphero Bolt Maze 
+//programmer Team: Andrew Krusniak & Jacob Harback 
+//Date:2.6.2023
+//project:sphero Bolt 
+
+
+//blue 
 
 async function startProgram() { 
-	await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true)
-	setSpeed(80); 
-	await delay (2); 
-	setSpeed(0); 
-	await roll((getHeading() + 90), 60, 0); 
-	setMainLed({r: 0, g: 0, b: 255 }); 
+    await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true)
+    setSpeed(70); 
+    await delay (2); 
+    setSpeed(0); 
+    setHeading(90);
+    setMainLed({r: 0, g: 0, b: 255 });  
+    
+} 
 
-
-
-
+//sound 1
