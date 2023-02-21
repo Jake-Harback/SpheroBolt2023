80d3afb7fb61e8442c09f00a9bd01306530a41b2
Jake Harback <01jharback@gmail.com>
2023-02-21 07:51:26 -0500

    2.20.23

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,74 +1,87 @@
-//programmer Team: Andrew Krusniak & Jacob Harback 
+//programmer Team:Andrew Krusniak Jacob Harback
 //Date:2.6.2023
-//project:sphero Bolt 
+//project:sphero Bolt
 
 
-//blue 
+
 
 async function CheckpointBLUE() {
-  await scrollMatrixText('START', { r: 255, g: 0 , b: 0 }, 30, true);
+  await scrollMatrixText('START', { r: 255, g: 255, b: 255 }, 30, true);
   await roll(0, 78.5, 2);
-  await spin(90,2);
+  await spin(90, 2.5);
   setMainLed({ r: 0, g: 0, b: 255 })
 }
-    
 
-
-//sound 1 
 
 /**
  *
 */
-async function soundnumber1(){
-  await roll(90, 69, 1);
-  await delay(3);
+async function CheckpointNoise_1(){
+  await roll(90, 64.5, 1.5);
+  await delay(2);
   await roll(180, 65, 1);
-  await spin(42, 2);
-  await Sound.Effects.Fireworks.play(true) 
+  await spin(42.5, 2.5);
+  await Sound.Effects.Fireworks.play();
 }
 
-//red 
 
 /**
  *
 */
-async function Change to RED() {
-await roll(217.5, 56.75, 1);
-await delay(3);
-await roll(132.5, 41, 1.3);
+async function CheckpointRED() {
+await roll(217.5, 49.5, 1.5);
+await delay(2);
+await roll(132.5, 47.5, 1);
 setMainLed({ r: 255, g: 0, b: 0})
-await delay(3);
+await delay(2);
 }
 
-//sound 2
 
 /**
  *
 */
-async function Soundnumber2() {
-await roll(42.5, 55.75, 1);
-await delay(2.5);
-await roll(130, 40.5, 1.75);
-await Sound.Effects.Thunder.play(true)
-
-}  
-
-/**
- *
-*/
-async function Change to green() {
+async function CheckpointNoise_2() {
+await roll(42.5, 48.5, 1.5);
 await delay(1.5);
-await roll(180, 43.75, 1);
-await delay(2.5);
+await roll(130, 33.5, 1.5);
+await Sound.Effects.Thunder.play();
+}
+
+
+/**
+ *
+*/
+async function CheckpointGREEN() {
+await delay(1.5);
+await roll(180, 42.5, 1);
+await delay(1.5);
 await roll(89.5, 41, 1.1);
 setMainLed({ r: 0, g: 255, b: 0})
 }
 
+
+/**
+ *
+*/
+async function CheckpointNoise_3() {
+await roll(89.5, 41, 1.1);
+await delay(2);
+await roll(0, 45, 1.5);
+await delay(2);
+await roll(315, 45, 1.25);
+await Sound.Mechanical.TruckHorn.play();
+await delay(2);
+}
+
+
+/**
+ *
+*/
 async function startProgram() {
-await Change to BLUE(); // Calls CheckpointBLUE
-await Soundnumber_1();
-await Change to RED();
-await Soundnumber_2();
-await Change to GREEN();
-await Soundnumber_3();
+await CheckpointBLUE(); // Calls CheckpointBLUE
+await CheckpointNoise_1();
+await CheckpointRED();
+await CheckpointNoise_2();
+await CheckpointGREEN();
+await CheckpointNoise_3();
 }
